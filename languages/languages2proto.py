c6bc6744711a750e82f1b2d79a8134d10e1c41e5
David Goffredo <davidg@okcupid.com>
2020-08-21 23:59:00 -0400

    protobuf enum, base-26

---
+++ languages/languages2proto.py
@@ -0,0 +1,31 @@
+import sqlite3
+import sys
+
+db = sqlite3.connect(sys.argv[1])
+
+# See https://iso639-3.sil.org/code_tables/download_tables for more information
+query = """
+select distinct Id, Ref_Name
+from Language
+where Scope = 'I'                  -- (I)ndividual
+  and Language_Type in ('L', 'C'); -- (L)iving or (C)onstructed
+"""
+
+def id_from_alpha3(alpha3):
+    """Return a positive integer ID corresponding to the three-letter language
+    code `alpha3`. The ID is the number resulting from interpreting `alpha2`
+    as base-26 big-endian unsigned integer."""
+    alpha3 = alpha3.upper()
+    high, mid, low = alpha3
+    origin = ord('A')
+    high, mid, low = ord(high) - origin, ord(mid) - origin, ord(low) - origin
+    base = 26
+    return high * base**2 + mid * base**1 + low * base**0
+
+
+print('enum Language {')
+print('  LANGUAGE_UNSPECIFIED = 0;')
+for alpha3, name in db.execute(query):
+    print(f'  LANGUAGE_{alpha3.upper()} = {id_from_alpha3(alpha3)}; // {name}')
+
+print('}')
